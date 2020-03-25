dc96b3370144eec9fcd40afcfc63c7172eb24114
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-03-25 09:55:28 -0400

    Create ez-mpy.py

---
+++ ez-mpy.py
@@ -0,0 +1,10 @@
+#Add this line to the bottom of your code
+#import ez-mpy.py
+#to make sure ez-mpy.py runs after the code is finished
+#NOTE: THIS HAS BEEN REMOVED BY AN IMPROVMENT
+import sys
+if __name__ == '__main__':
+   print('Setup Mode...')
+   print('On your system should be a drive named EZMPY_READY when this is done')
+   print('Code not implemented. Exiting...')
+   sys.exit(0)
